bd29ddb0af85fd2433839c71faf66f1fe151d681
cybon <cybon@192.168.178.67>
2018-03-13 00:03:14 +0100

    Week 1 Discussion on class variable and methods.

---
+++ Week1/src/Week1.java
@@ -0,0 +1,15 @@
+
+public class Week1 
+{
+    // Instance Variables
+    static int a=10;
+    static int b=50;
+    
+    // Main() is a static method
+    public static void main (String[] args)
+    {
+        System.out.println(a+b);  
+                                 
+    }
+    
+}
