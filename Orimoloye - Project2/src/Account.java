dc180917e9890e5f09bfb4816f28416c0ce0b78c
cybon <cybon@192.168.178.67>
2018-04-07 02:43:27 +0200

    CMIS 242 - Project 2 The ATM Program.

---
+++ Orimoloye - Project2/src/Account.java
@@ -0,0 +1,70 @@
+/**
+ * @author Mark Orimoloye
+ * Handles account transactions
+ */
+public class Account 
+{
+    private double balance;
+    private int withdrawals;
+    
+    //crate account constructor
+    public Account(double initialBalance)
+    {
+        balance = initialBalance;
+        withdrawals = 0;
+    }
+    
+    //get the balance
+    public double getBalance()
+    {
+        return balance;
+    }
+    
+    //make a deposit
+    public void deposit(double amount)
+    {
+        balance +=amount;
+    }
+    
+    //check balance
+    public void withdraw(double amount) throws InsufficientFunds 
+    {
+        if (amount > balance)
+        {
+            throw new InsufficientFunds("Insufficient funds");
+        }
+        else 
+        {
+            balance -= amount;
+            withdrawals++;
+        }
+    }
+    
+    //number of withdrawals
+    public int getWithdrawals()
+    {
+        return withdrawals;
+    }
+    
+    //add service fee
+    public void serviceFee() throws InsufficientFunds
+    {
+        if (1.5 > balance)
+        {
+            throw new InsufficientFunds("Insufficient Funds");
+        }
+        else
+        {
+            balance -= 1.5;
+        }
+    }
+    
+    public void transfer(double amount, Account toAccount) throws InsufficientFunds
+    {
+        //withdraw from this account
+        this.withdraw(amount);
+        
+        //deposit to the other account
+        toAccount.deposit(amount);
+    }
+}
---
+++ Orimoloye - Project2/src/InsufficientFunds.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Mark Orimoloye
+ * Handles all exceptions
+ */
+public class InsufficientFunds extends Exception 
+{
+    public InsufficientFunds()
+    {
+        // If account balance is less than 0, throw Exception.
+        JOptionPane frame = new JOptionPane();
+        JOptionPane.showMessageDialog(frame, "You have insufficient funds.");
+    }
+    
+    public InsufficientFunds(String e)
+    {
+        // If account balance is less than 0, throw Exception.
+        JOptionPane frame = new JOptionPane();
+        JOptionPane.showMessageDialog(frame, "You have insufficient funds.");
+    }
+}
