404c5fb24b5d605d4b60dac35181ebf81c84ffc7
Mark Orimoloye <cybonix@gmail.com>
2018-05-06 17:36:21 +0200

    Orimoloye - Week 8 Discussion

---
+++ Orimoloye - Week8/src/Week8.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * @author Joseph Mark Orimoloye
+ * Week 8 - Converting an array into a list and back.
+ * 06 May 2018
+ */
+public class Week8 
+{
+    public static void main(String[] args) 
+    {
+           String[] candies = {"Blowpops", "Now-and-Laters","Lemonheads","Skittles" };
+           
+           System.out.println("Candies array before list conversion and additions: \n");
+           for(String x : candies)
+               System.out.printf("%s ", x);
+           
+           // Converting the candies array into a linked list
+           LinkedList<String> thelist = new LinkedList<String>(Arrays.asList(candies));
+           
+           // Now, we can use list and collection methods on the array... Cool~!
+           thelist.add("Jawbreakers");
+           thelist.addFirst("Redhots");
+           
+           // Convert back to an array... watch this!
+           candies = thelist.toArray(new String[thelist.size()]);
+           
+           System.out.println("\n\n\n Candies array after the list conversion and additions: \n");
+           
+           for(String x : candies)
+               System.out.printf("%s ", x);
+    }
+}
